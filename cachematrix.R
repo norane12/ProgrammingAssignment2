9d3b3c4ddb677c0226b53796e05fb4c59c5b46f3
norane12 <virginie.ory@gmail.com>
2015-03-22 17:19:54 -0400

    final edit version 2 cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -9,16 +9,20 @@
 ## 3. function to set the value of the inverse of the matrix
 ## 4. function to get the value of the inverse of the matrix
  
-makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {                           ## 1. function to set the value of the matrix
-                x <<- y
-                m <<- NULL
+makeCacheMatrix <- function(x = matrix()) {            ## Initialization of the function which requires a matrix as its only argument
+        m <- NULL                                      ## Initializes the 'inverse' variable to  NULL
+        set <- function(y) {                           ## 1. Create a function "set" to set the value of the matrix
+                x <<- y                                ## Sets 'x' for the function environment to 'y'
+                m <<- NULL                             ## Sets 'm' for the 'makeCacheMatrix' environment to NULL
         }
-        get <- function() x                            ## 2. function to get the value of the matrix
-        setinverse <- function(inverse) m <<- inverse  ## 3. function to set the value of the inverse of the matrix
-        getinverse <- function() m                     ## 4. function to get the value of the inverse of the matrix
-        list(set = set,                                ## Returns a list containing 1., 2., 3. and 4.
+        get <- function() x                            ## 2. Creates a function "get" in the 'makeCacheMatrix' parent 
+                                                       ## and assigns the matrix to it
+        setinverse <- function(inverse) m <<- inverse  ## 3. Creates a function "setinverse" that takes the value 'inverse' 
+                                                       ## and sets it to the value of 'm' in the 'makeCacheMatrix' frame
+        getinverse <- function() m                     ## 4. Creates a function "getinverse" that returns the value of "m" 
+                                                       ## from the 'makeCacheMatrix' frame
+        list(set = set,                                ## Lists out the values of the functions 1., 2., 3. and 4 
+                                                       ## in the 'makeCacheMatrix' frame
              get = get,                     
              setinverse = setinverse,      
              getinverse = getinverse)      
@@ -30,14 +34,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Otherwise, it calculates the inverse and sets its value in the cache using the "setinverse" function.
 ## The function "cacheSolve" assumes that the matrix is always invertible.
 
-cacheSolve <- function(x, ...) {
-        m <- x$getinverse()
+cacheSolve <- function(x, ...) {                 ## Initialization of the function which requires a matrixCacheMatrix object
+        m <- x$getinverse()                      ## Goes to the 'x' environment and assigns the 'm' value from the 'x' environment 
+                                                 ## to this one 
         if(!is.null(m)) {                        ## If the old cache value DOES exist
                 message("getting cached data")   ## Prints the message "getting cached data"
                 return(m)                        ## Returns the value of the inverse from the cache
         }
-        data <- x$get()                          ## If the old cache value DOES NOT exist, it gets the value of the matrix
-        m <- solve(data,...)                     ## Calculates the inverse of the matrix using the "solve" function
+        data <- x$get()                          ## If the old cache value DOES NOT exist, it pulls the value of the matrix 
+                                                 ## into a local variable named 'data'
+        m <- solve(data,...)                     ## Calculates the inverse of the matrix on the 'data' local variable  
+                                                 ## using the "solve" function                                          
         x$setinverse(m)                          ## Uses the "setinverse" function to set the inverse of the matrix in the cache 
         m                                        ## Returns the value of the inverse of the matrix
 }
